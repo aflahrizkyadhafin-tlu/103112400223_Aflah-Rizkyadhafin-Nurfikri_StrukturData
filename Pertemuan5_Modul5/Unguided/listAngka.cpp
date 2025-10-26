a7d56e12cacf04c1e12a934b0d9d211a43efdbe3
FYIF <aflahrizkyadhafin@student.telkomuniversity.ac.id>
2025-10-26 10:55:39 +0700

    Modul 5 UNG main.cpp & listAngka.cpp

---
+++ Pertemuan5_Modul5/Unguided/listAngka.cpp
@@ -0,0 +1,39 @@
+#include "listAngka.h"
+
+void CreateList(List &L)
+{
+}
+
+address alokasi(infotype x)
+{
+    return address();
+}
+
+void dealokasi(address &node){
+
+}
+
+void printList(List L)
+{
+}
+
+void insertFirst(List &L, address nodeBaru)
+{
+}
+
+void insertLast(List &L, address nodeBaru)
+{
+}
+
+void insertAfter(List &L, address nodeBaru, address nodePrev)
+{
+}
+
+int nbList(List L)
+{
+    return 0;
+}
+
+void delAfter(List &L, address nodeHapus, address nodePrev)
+{
+}
---
+++ Pertemuan5_Modul5/Unguided/main.cpp
@@ -0,0 +1,26 @@
+#include "listAngka.h"
+
+int main(){
+    address nodeA, nodeB, nodeC, nodeD, nodeE, nodeF;
+    List linkedList;
+    CreateList(linkedList);
+
+    nodeA = alokasi(infotype({angka: 8, Next:nullptr}));
+    nodeB = alokasi(infotype({angka: 13, Next:nullptr}));
+    nodeC = alokasi(infotype({angka: 18, Next:nullptr}));
+    nodeD = alokasi(infotype({angka: 25, Next:nullptr}));
+    nodeE = alokasi(infotype({angka: 33, Next:nullptr}));
+    nodeF = alokasi(infotype({angka: 40, Next:nullptr}));
+
+    insertFirst(linkedList,nodeB);
+    insertLast(linkedList, nodeA);
+    insertAfter(linkedList,nodeD,nodeB);
+    insertFirst(linkedList,nodeC);
+    insertLast(linkedList,nodeE);
+    insertFirst(linkedList,nodeF);
+
+    delAfter(linkedList, nodeB, nodeC);
+    printList(linkedList);
+    
+    return 0;
+}
